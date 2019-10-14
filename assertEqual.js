969732cc40a084ba0dbe7842f5196c21ee2b96cd
Ted Ogmundson <togmund@gmail.com>
2019-10-14 23:09:18 +0000

    commit of the linted assertEqual.js function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual === expected
+    ? console.log("✅ Assertion Passed: " + actual + " === " + expected)
+    : console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, "1");
+assertEqual(true, false);
+assertEqual(1 + 1, 2);
