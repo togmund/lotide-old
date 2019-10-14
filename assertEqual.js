5ec8a14959cdbc5766995438e12636a1cf10e294
Ted Ogmundson <togmund@gmail.com>
2019-10-14 23:20:28 +0000

    updated assertEqual.js with Template Literals in place of concatencating strings and arguments

--- assertEqual.js
+++ assertEqual.js
@@ -2,8 +2,8 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   actual === expected
-    ? console.log("✅ Assertion Passed: " + actual + " === " + expected)
-    : console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+    ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
