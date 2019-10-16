e5d6341cc634a51251874fcd616bb96536edd9c3
Ted Ogmundson <togmund@gmail.com>
2019-10-16 02:20:06 +0000

    commited solution ot eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+
+// assertEqual FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual === expected
+    ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// head FUNCTION IMPLEMENTATION
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
