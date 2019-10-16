d0d725ac45835f5188a777e33c5a3d1583654587
Ted Ogmundson <togmund@gmail.com>
2019-10-16 23:11:10 +0000

    final implementation of eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -9,9 +9,23 @@ const assertEqual = function (actual, expected) {
 /*
 Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
 */
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const findKeyByValue = function (objectToScan, valueToFind) {
   for (let key in objectToScan) {
+    if (Array.isArray(objectToScan[key]) && Array.isArray(valueToFind)) {
+      return eqArrays(objectToScan[key], valueToFind);
+    }
     if (objectToScan[key] === valueToFind) return key;
   }
 };
@@ -19,6 +33,9 @@ const findKeyByValue = function (objectToScan, valueToFind) {
 const eqObjects = function (objectOne, objectTwo) {
   for (let key in objectOne) {
     if (!findKeyByValue(objectTwo, objectOne[key])) return false;
+  }
+  for (let key in objectTwo) {
+    if (!findKeyByValue(objectOne, objectTwo[key])) return false;
   }
   return true;
 };
@@ -31,3 +48,10 @@ assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
