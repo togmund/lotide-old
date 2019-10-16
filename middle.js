8cde34c6856e7b330e4796df91271c1934b90df7
Ted Ogmundson <togmund@gmail.com>
2019-10-16 02:54:04 +0000

    updated comments on assertArrasyEqual.js and created the wireframe for middle.js

--- middle.js
+++ middle.js
@@ -18,8 +18,9 @@ const assertArraysEqual = function (actual, expected) {
     : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
+// middle FUNCTION IMPLEMENTATION
+const middle = function (args) {
+  return;
+};
+
 // Tests
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
