75f74b95b28145c2af4454b3494eb17bd9784da4
Ted Ogmundson <togmund@gmail.com>
2019-10-16 03:21:09 +0000

    implimented final middle function with some very ugly math

--- middle.js
+++ middle.js
@@ -1,3 +1,4 @@
+// Testing Functions
 // eqArrays FUNCTION IMPLEMENTATION
 const eqArrays = function (arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
@@ -18,9 +19,26 @@ const assertArraysEqual = function (actual, expected) {
     : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
+// Actual Function
 // middle FUNCTION IMPLEMENTATION
-const middle = function (args) {
-  return;
+const middle = function (argArray) {
+  if (argArray.length < 3) {
+    return argArray.slice(0, 0);
+  }
+  if (argArray.length % 2 !== 0) {
+    return argArray.slice(
+      (argArray.length / 2) - 0.5,
+      (argArray.length / 2) - 0.5 + 1);
+  }
+  if (argArray.length % 2 === 0) {
+    return argArray.slice(
+      argArray.length / 2 - 1,
+      argArray.length / 2 + 1
+    );
+  }
 };
 
 // Tests
+assertArraysEqual(middle([0, 2, 6, 8, 10]), [6]); // => true
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => true
+assertArraysEqual(middle([0, 78567]), []); // => true
