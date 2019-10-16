bf71d0846e029553620adaa69c8eff6fe307348a
Ted Ogmundson <togmund@gmail.com>
2019-10-16 22:39:03 +0000

    working, linted version of findKeyByValue committed.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,5 @@
 // TEST IMPLEMENTATION
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   actual === expected
     ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
     : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
@@ -10,33 +10,18 @@ const assertEqual = function (actual, expected) {
 Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
 */
 
-const findKeyByValue = function (objectToScan, valueToFind) {
-  // const results = {};
-  // for (let i = 0; i < stringToCount.length; i++) {
-  //   if (results[stringToCount[i]]) {
-  //     results[stringToCount[i]].push(i);
-  //   } else {
-  //     results[stringToCount[i]] = [i];
-  //   }
-  // }
-  // delete results[' '];
-  // return results;
-}
+const findKeyByValue = function(objectToScan, valueToFind) {
+  for (let key in objectToScan) {
+    if (objectToScan[key] === valueToFind) return key;
+  }
+};
 
 // // TESTS
-// letterPosition("lighthouse in the house") would return:
-// {
-//   l: 1,
-//   i: 2,
-//   g: 1,
-//   h: 4,
-//   t: 2,
-//   o: 2,
-//   u: 2,
-//   s: 2,
-//   e: 3,
-//   n: 1,
-// }
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
 
-console.log(letterPosition("lighthouse in the house"));
-console.log(letterPosition("antidisestablishmentarianism"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
