79e41d1abe0e5f8ad538d8b7e39f8aed6b7cee9b
Ted Ogmundson <togmund@gmail.com>
2019-10-17 23:30:43 +0000

    solution to findKey with logging and one test

--- findKey.js
+++ findKey.js
@@ -21,24 +21,29 @@ const assertArraysEqual = function (actual, expected) {
 
 // Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
 
-const findKey = function (array, callback) {
+const findKey = function (obj, callback) {
   const results = [];
-  for (let item of array) {
-    if (callback(item)) return results;
-    results.push(item);
+  for (let key in obj) {
+    console.log(key);
+    console.log(obj[key]);
+    console.log(obj[key].stars);
+    console.log(obj[key].stars === 2);
+    if (callback(obj[key])) {
+      console.log("I pushed", key);
+      results.push(key);
+    }
   }
   return results;
 }
 
 // // Tests
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
-
-// console.log('---');
-
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// assertArraysEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']);
-// // // Expected Output
-// // [ 1, 2, 5, 7, 2 ]
-// // ---
-// // [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+console.log(
+  findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri": { stars: 3 },
+    "noma": { stars: 2 },
+    "elBulli": { stars: 3 },
+    "Ora": { stars: 2 },
+    "Akelarre": { stars: 3 }
+  }, x => x.stars === 2)
+)// => "noma"
