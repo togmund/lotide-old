659b20f8583006bdda4febe6f4e738f0ee571d7d
Ted Ogmundson <togmund@gmail.com>
2019-10-17 23:49:37 +0000

    switched to solve the actual problem of findKey and flattened tests for findKey.

--- findKey.js
+++ findKey.js
@@ -1,49 +1,28 @@
 // eqArrays FUNCTION IMPLEMENTATION
-const eqArrays = function (arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// assertArraysEqual FUNCTION IMPLEMENTATION
-const assertArraysEqual = function (actual, expected) {
-  eqArrays(actual, expected)
+const assertEqual = function (actual, expected) {
+  actual === expected
     ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
     : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-
 // Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
 
 const findKey = function (obj, callback) {
-  const results = [];
   for (let key in obj) {
-    console.log(key);
-    console.log(obj[key]);
-    console.log(obj[key].stars);
-    console.log(obj[key].stars === 2);
-    if (callback(obj[key])) {
-      console.log("I pushed", key);
-      results.push(key);
-    }
+    if (callback(obj[key])) return key;
   }
-  return results;
-}
+};
 
 // // Tests
-console.log(
-  findKey({
-    "Blue Hill": { stars: 1 },
-    "Akaleri": { stars: 3 },
-    "noma": { stars: 2 },
-    "elBulli": { stars: 3 },
-    "Ora": { stars: 2 },
-    "Akelarre": { stars: 3 }
-  }, x => x.stars === 2)
-)// => "noma"
+const dinerRatings = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+};
+
+assertEqual(findKey(dinerRatings, x => x.stars === 2), 'noma');
+assertEqual(findKey(dinerRatings, x => x.stars === 3), 'Akaleri');
+assertEqual(findKey(dinerRatings, x => x.stars === 4), undefined);
