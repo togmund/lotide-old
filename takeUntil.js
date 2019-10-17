f4b4f422d6996e113877372d0530822ead04b86c
Ted Ogmundson <togmund@gmail.com>
2019-10-17 23:14:22 +0000

    final takeUntil function and tests

---
+++ takeUntil.js
@@ -0,0 +1,48 @@
+// eqArrays FUNCTION IMPLEMENTATION
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// assertArraysEqual FUNCTION IMPLEMENTATION
+const assertArraysEqual = function (actual, expected) {
+  eqArrays(actual, expected)
+    ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// The function will return a "slice of the array with elements taken from the beginning." It should keep going until the callback/predicate returns a truthy value.
+// To keep things simple, the callback should only be provided one value: The item in the array.
+// Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
+
+const takeUntil = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) return results;
+    results.push(item);
+  }
+  return results;
+}
+
+// Expected Input
+// For illustration purposes, here are some examples of how our function could be used and what the expected results would look like.
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']);
+// // Expected Output
+// [ 1, 2, 5, 7, 2 ]
+// ---
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
